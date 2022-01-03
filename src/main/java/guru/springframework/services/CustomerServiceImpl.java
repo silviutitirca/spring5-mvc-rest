ada56cd575af89a9f62fcc146e0d6daa2421c63f
silviutitirca <s_titirca@yahoo.com>
2022-01-03 12:37:02 +0100

    Added function to patch a customer via api.

--- src/main/java/guru/springframework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/services/CustomerServiceImpl.java
@@ -70,4 +70,20 @@ public class CustomerServiceImpl implements CustomerService {
         customer.setId(id);
         return saveAndReturnDTO(customer);
     }
+
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+        return customerRepository.findById(id).map(customer -> {
+
+            if(customerDTO.getFirstName() != null){
+                customer.setFirstName(customerDTO.getFirstName());
+            }
+
+            if(customerDTO.getLastName() != null){
+                customer.setLastName(customerDTO.getLastName());
+            }
+
+            return customerMapper.customerToCustomerDTO(customerRepository.save(customer));
+        }).orElseThrow(RuntimeException::new);
+    }
 }
